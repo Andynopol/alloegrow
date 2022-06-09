f03a880921048d6c5ba36ea10d011e37ffc8a0d2
Andynopol <andrei.nechita95@gmail.com>
2022-06-09 14:09:49 +0300

    depl try

--- APP/src/index.ts
+++ APP/src/index.ts
@@ -10,14 +10,15 @@ const PORT = process.env.PORT || 3005;
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const app = express();
 
+app.use( `/`, express.static( path.join( __dirname, `../pubilc` ) ) );
 app.use( express.json( { limit: '1mb' } ) );
 app.use( express.urlencoded( { limit: '30mb', extended: true } ) );
-
 app.use( '/usr', userRouter );
 app.use( '/planifications', planificationRouter );
 
+
 app.get( '/', ( _req: Request, res: Response ) => {
-    res.sendFile( path.join( __dirname, '../public', 'index.html' ) );
+    res.sendFile( path.resolve( __dirname, '../public', 'index.html' ) );
 } );
 
 app.listen( PORT, () => console.log( `listening on port: ${ PORT }` ) );
