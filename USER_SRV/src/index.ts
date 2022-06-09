d352e34ce67879ab799a6abf89e2e8dc69a449da
Andynopol <andrei.nechita95@gmail.com>
2022-06-09 12:50:26 +0300

    Package.json

--- USER_SRV/src/index.ts
+++ USER_SRV/src/index.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import { config } from 'dotenv';
 import userRouter from './router/userRouter.js';
 import listenerRouter from './router/listenerRouter.js';
-import cors from 'cors';
 
 
 config();
@@ -15,7 +14,7 @@ app.use( urlencoded( { limit: '30mb', extended: true } ) );
 
 //routes
 
-app.use( '/usr', cors(), userRouter );
+app.use( '/usr', userRouter );
 app.use( '/events', listenerRouter );
 
 app.listen( PORT, () => console.log( `listening at port: ${ PORT }` ) );
